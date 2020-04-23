60ac456bd934743edd5a0de613ba91f1b290f861
VishwantReddyMamidi <mamidivishwant@gmail.com>
2020-04-23 14:19:07 -0400

    first commit

---
+++ ParkingBooking.WCF/IService1.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace ParkingBooking.WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        IEnumerable<BookingsTable> GetBookings(bool get);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    public class BookingsTable
+    {
+        [DataMember]
+        public Guid BookingId { get; set; }
+        [DataMember]
+        public string FullName { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
+        [DataMember]
+        public string CarPlate { get; set; }
+        [DataMember]
+        public string BookedDates { get; set; }
+        [DataMember]
+        public int NumberofDaysBooked { get; set; }
+        [DataMember]
+        public DateTime? BookedOn { get; set; }
+
+        //[DataMember]
+        //public virtual PaymentTranscationTable PaymentTranscationTable { get; set; }
+    }
+
+
+    //public partial class PaymentTranscationTable
+    //{
+    //    [DataMember]
+    //    public Guid BookingId { get; set; }
+    //    public string PaymentTranscationId { get; set; }
+    //    public int AmountPaid { get; set; }
+    //    public string StripResponse { get; set; }
+    //    public Guid Id { get; set; }
+
+    //    [JsonIgnore]
+    //    public virtual BookingsTable Booking { get; set; }
+    //}
+}
---
+++ ParkingBooking.WCF/Service1.svc.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ParkingBooking.WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string connectionString;
+        public Service1()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            connectionString = connStr;
+        }
+        public IEnumerable<BookingsTable> GetBookings(bool get)
+        {
+            if(get==false)
+            {
+                return null;
+            }
+            else
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"SELECT [BookingID]
+                                              ,[FullName]
+                                              ,[Email]
+                                              ,[Phone]
+                                              ,[CarPlate#]
+                                              ,[BookedDates]
+                                              ,[NumberofDaysBooked]
+                                              ,[BookedOn]
+                                          FROM [dbo].[BookingsTable]";
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    BookingsTable[] pocos = new BookingsTable[100];
+                    int index = 0;
+                    while(reader.Read())
+                    {
+                        BookingsTable poco = new BookingsTable();
+
+                        poco.BookingId = reader.GetGuid(0);
+                        poco.FullName = reader.GetString(1);
+                        poco.Email = reader.GetString(2);
+                        poco.Phone = reader.GetString(3);
+                        poco.CarPlate = reader.GetString(4);
+                        poco.BookedDates = reader.GetString(5);
+                        poco.NumberofDaysBooked = reader.GetInt32(6);
+                        poco.BookedOn = reader.GetDateTime(7);
+
+                        pocos[index] = poco;
+                        index++;
+
+                    }
+
+                    conn.Close();
+
+                    return pocos.Where(a => a != null).ToList();
+                }
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue = $"Hello {composite.StringValue}";
+            }
+            return composite;
+        }
+
+    }
+}
